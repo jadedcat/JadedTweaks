bc5577504c6a7957bf2a2a44c8a9fe84ee674635
jadedcat <jadedcatMC@gmail.com>
2015-05-21 15:47:08 -0500

    Testing Thaumcraft additions

--- main/java/com/jadedpacks/jctweaks/helpers/ThaumcraftHelper.java
+++ main/java/com/jadedpacks/jctweaks/helpers/ThaumcraftHelper.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import cpw.mods.fml.common.Loader;
 import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.ThaumcraftApiHelper;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.crafting.CrucibleRecipe;
@@ -34,21 +35,58 @@ public class ThaumcraftHelper {
 		if (Loader.isModLoaded("ThaumCraft"))
 		{
 			removeRecipes();
-			getComponents();
-			addRecipes();
 			setupResearch();
+			initAspects();
+			addAspects();
+			addRecipes();
 		}
 	}
 
+	public static Aspect PURPLE;
+	public static void initAspects()
+	{
+		PURPLE = new Aspect("purpura", 0xB68CFF, new Aspect[]{Aspect.PLANT, Aspect.TREE}, new ResourceLocation("jctweaks", "textures/aspects/purpura.png"),1);
+
+	}
+
+	private static AspectList getAspectList(ItemStack stack){
+		AspectList list = ThaumcraftApiHelper.getObjectAspects(stack);
+		return list !=null ? list : new AspectList();
+	}
+
+	public static void addAspects(){
+		AspectList list;
+
+		list = getAspectList(Parts.amethyst);
+		list.add(PURPLE, 4);
+		ThaumcraftApi.registerObjectTag (Parts.amethyst, list);
+
+		ThaumcraftApi.registerObjectTag("listAllseed", (new AspectList()).add(Aspect.CROP, 4).add(Aspect.HARVEST, 3));
+
+
+
+	}
 
 	private static void setupResearch() {
 
-		//ArrayList<Object> list;
-		//ItemStack input;
-		//IRecipe recipe;
-		//String category = "AGRIMANCY";
-		//ResearchCategories.registerCategory(category, new ResourceLocation("thermalfoundation", "textures/blocks/ore/Ore_Mithril.png"),
-		//new ResourceLocation("thaumcraft", "textures/gui/gui_researchback.png"));
+		//Categories
+
+		ResearchCategories.registerCategory("AGRIMANCY", new ResourceLocation("jctweaks", "textures/aspects/purpura.png"), new ResourceLocation("thaumcraft", "textures/gui/gui_researchback.png"));
+
+		(new ResearchItem("RECALLSTONE", "AGRIMANCY", (new AspectList()).add(Aspect.MAGIC, 3).add(Aspect.MOTION, 2), 2, 0, 2,
+				(Parts.recallStone))).setPages(new ResearchPage[]
+						{ new ResearchPage("jctweaks.research_page.RECALLSTONE"), new ResearchPage((IArcaneRecipe)
+								ThaumcraftApi.addShapelessArcaneCraftingRecipe("Recall Stone", Parts.recallStone, (new AspectList()).add(Aspect.EARTH, 5).add(Aspect.ORDER, 3), "SES", "EDE", "SES", Character.valueOf('B'), Parts.stone, Character.valueOf('E'), Parts.epearl, Character.valueOf('R'), Parts.diamond)) }).setParents(new String[] { "RESEARCH" }).registerResearchItem();
+
+
+		(new ResearchItem("DIMENSIONSTONE", "AGRIMANCY", (new AspectList()).add(Aspect.ELDRITCH, 3).add(Aspect.MOTION, 2), 2, -1, 2,
+				(Parts.dimensionStone))).setPages(new ResearchPage[]
+						{ new ResearchPage("jctweaks.research_page.DIMENSIONSTONE"), new ResearchPage((IArcaneRecipe)
+								ThaumcraftApi.addShapelessArcaneCraftingRecipe("Dimension Stone", Parts.dimensionStone, (new AspectList()).add(Aspect.ELDRITCH, 5).add(Aspect.ORDER, 3), "BEB", "ERE", "BEB", Character.valueOf('B'), Parts.blazerod, Character.valueOf('E'), Parts.epearl, Character.valueOf('R'), Parts.recallStone)) }).setParents(new String[] { "RECALLSTONE" }).registerResearchItem();
+
+
+
+
 
 	}
 	private static void removeRecipes() {
@@ -61,20 +99,9 @@ public class ThaumcraftHelper {
 		}
 
 	}
-	private static void getComponents() {
-
-	}
-	private static void addRecipes() {
-
-		//ThaumcraftApi.addArcaneCraftingRecipe(null, null, null, null);
-		//for (ItemStack seeds : OreDictionary.getOres("listAllSeeds"){ Aspects.add(seeds, ...) }
-
-
-
-	}
+	private static void addRecipes() {}
 	public static void postInit() {
-		//Aspect purple = new Aspect("purpura", 0xB68CFF, new Aspect[] { Aspect.BEAST, Aspect.SENSES },
-		//new ResourceLocation("jctweaks", "textures/aspects/purpura.png"), 1);
+
 
 	}
 }
